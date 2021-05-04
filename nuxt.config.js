87b27a7913fbed5cf1d3366ffd0168d2818999a7
Usman Irale <superirale@gmail.com>
2021-05-04 16:11:40 +0100

    project setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/site/'
+    base: '/usman.github.io/'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
