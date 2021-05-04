ddc78cc314c25ff2fd37e3d124e352657b78c847
Usman Irale <superirale@gmail.com>
2021-05-04 15:23:22 +0100

    project setup

--- components/Header.vue
+++ components/Header.vue
@@ -8,8 +8,8 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand name hidden-xs" href="/">Usman Irale</a>
-          <a class="navbar-brand name visible-xs" href="/">UI</a>
+          <a class="navbar-brand name hidden-xs" href="/site/">Usman Irale</a>
+          <a class="navbar-brand name visible-xs" href="/site/">UI</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav pull-right hidden-xs">
