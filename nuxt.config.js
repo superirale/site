0548d08682b63e5208d714e7b832fc626e732bf0
Usman Irale <superirale@gmail.com>
2021-05-04 16:12:45 +0100

    change site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: 'Usman Irale',
     htmlAttrs: {
       lang: 'en'
     },
